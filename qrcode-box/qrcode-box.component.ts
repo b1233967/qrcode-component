c227392e935f8080592dbff4c1eeb9c9cd5aa3dd
Tay Cheng <b1233967@gmail.com>
2022-02-15 10:34:30 +0800

    Auto-generated commit [ci skip]

---
+++ qrcode-box/qrcode-box.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-qrcode-box',
+  templateUrl: './qrcode-box.component.html',
+  styleUrls: ['./qrcode-box.component.scss']
+})
+export class QrcodeBoxComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
